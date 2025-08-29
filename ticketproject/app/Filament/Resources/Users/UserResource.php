e7644840924a3ed7fcb9c41faa633d46d4308e3b
KAPEX25 <metedi252506@gmail.com>
2025-08-30 00:32:31 +0300

    Admin Panel Users Eklendi

---
+++ ticketproject/app/Filament/Resources/Users/UserResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources\Users;
+
+use App\Filament\Resources\Users\Pages\CreateUser;
+use App\Filament\Resources\Users\Pages\EditUser;
+use App\Filament\Resources\Users\Pages\ListUsers;
+use App\Filament\Resources\Users\Schemas\UserForm;
+use App\Filament\Resources\Users\Tables\UsersTable;
+use App\Models\User;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use App\Models\Post;
+use Filament\Actions\Action;
+
+use Filament\Support\Enums\IconSize;
+use Filament\Support\Facades\FilamentIcon;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'User';
+
+
+    public static function form(Schema $schema): Schema
+    {
+        return UserForm::configure($schema)
+        ->schema([
+            TextInput::make('name')
+                ->label('Full Name')
+                ->required()
+                ->maxLength(20)
+                ->minLength(3),
+            TextInput::make('email')
+                ->label('E-Mail')
+                ->required()
+                ->email(),
+            TextInput::make('password')
+                ->label('Password')
+                ->required()
+                ->password(),
+        ]);
+        
+    }
+
+    public static function table(Table $table): Table
+    {
+    return UsersTable::configure($table)
+        ->columns([
+            TextColumn::make('name')->label('Full Name'),
+            TextColumn::make('email')->label('E-Mail'),
+        ])
+        ->filters([
+            // filtre eklemek istersen buraya
+        ]);
+    }
+
+
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListUsers::route('/'),
+            'create' => CreateUser::route('/create'),
+            'edit' => EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- ticketproject/routes/web.php
+++ ticketproject/routes/web.php
@@ -1,44 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Ornek;
-use App\Http\Controllers\Dashyonetim;
-use App\Http\Controllers\Formyonet;
-use App\Http\Controllers\Veritabaniislem;
-use App\Http\Controllers\Modelislemleri;
-use App\Http\Controllers\Iletisim;
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Route::get('/deneme', function (){
-//    return view('deneme');
-//});
-
-Route::get("/phpturkey/{isim}", [Ornek::class,'test']);
-
-Route::get("/dashboard",[Dashyonetim::class,'site'])->name("mainweb");
-
-Route::get("/form", [Formyonet::class, 'gorunum']);
-
-Route::middleware('arakontrol')->post("/form-sonuc", [Formyonet::class, 'sonuc'])->name('sonuc');
-
-Route::get("/ekle", [Veritabaniislem::class, 'ekle']);
-
-Route::get("/duzenle", [Veritabaniislem::class, 'guncelle']);
-
-Route::get("/delete", [Veritabaniislem::class, 'delete']);
-
-Route::get("/listele",[Veritabaniislem::class, 'bilgiler']);
-
-Route::get("/listelemodel",[Modelislemleri::class, 'liste']);
-
-Route::get("/eklemodel",[Modelislemleri::class, 'ekle']);
-
-Route::get("/guncellemodel",[Modelislemleri::class, 'guncelle']);
-
-Route::get("/deletemodel",[Modelislemleri::class, 'delete']);
-
-Route::get("/iletisim",[Iletisim::class, 'index']);
-
-Route::post("/iletisim-sonuc",[Iletisim::class, 'ekleme'])->name('iletisimsonuc');
