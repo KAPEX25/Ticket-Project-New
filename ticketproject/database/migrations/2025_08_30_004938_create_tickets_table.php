51839ad28211db5e2db244aefe74e4513f10c08f
KAPEX25 <metedi252506@gmail.com>
2025-08-30 04:20:52 +0300

    Users Sayfası Eklendi/Ticket Databes Migrate

---
+++ ticketproject/database/migrations/2025_08_30_004938_create_tickets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('description')->nullable();
+            $table->enum('status', ['open', 'in_progress', 'on_hold', 'resolved', 'closed'])->default('open');
+            $table->enum('priority', ['low', 'medium', 'high', 'critical'])->default('low');
+            $table->enum('category', ['hardware', 'software', 'network', 'account', 'other'])->default('other');
+            $table->enum('impact', ['low', 'medium', 'high'])->default('low');
+            $table->enum('source', ['web', 'email', 'phone', 'chat'])->default('web');
+            $table->foreignId('assigned_user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('created_by_user_id')->constrained('users')->onDelete('cascade');
+            $table->dateTime('sla_due_date');
+            $table->dateTime('resolved_at')->nullable();
+            $table->json('attachments')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
